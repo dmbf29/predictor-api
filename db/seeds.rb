faa34a99b6ef6b8ae290cf474909201e8d6514fc
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-07 17:48:12 +0900

    creating competition, round 1, groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,10 +37,21 @@ groups = {
     { name: 'Portugal', abbrev: 'POR' }
   ]
 }
+
+puts 'Creating the Euros...'
+competition = Competition.find_or_create_by(name: 'Euro 2020', start_date: Date.new(2021, 6, 12), end_date: Date.new(2021, 7, 12))
+p competition.errors.full_messages if competition.errors.any?
+puts '.. created the Euros'
+
 puts 'Creating or finding first round...'
-first_round = Round.find_or_create_by(name: 'Group Stage', round: 1)
+first_round = Round.find_or_create_by(name: 'Group Stage', number: 1, competition: competition)
+p first_round.errors.full_messages if first_round.errors.any?
 puts "...#{Round.count} Total Rounds"
 
 puts 'Creating or finding groups...'
-Group.find_or_create_by(name: groups.keys, round: first_round)
+groups.each_key do |group_name|
+  p "...#{group_name}..."
+  group = Group.find_or_create_by(name: group_name, round: first_round)
+  p group.errors.full_messages if group.errors.any?
+end
 puts "...#{Group.count} Total Groups"
