ba1b3daf6cb13d0f29e2c1afe92e3f3dab9ec64b
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-07 16:42:24 +0900

    added more thorough validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,10 @@
 class Team < ApplicationRecord
-  # TODO: home and away teams are on a match
-  # has_many :matches
   has_many :affiliations
   has_many :groups, through: :affiliations
   validates :name, presence: true, uniqueness: true
   validates :abbrev, presence: true, uniqueness: true
+
+  def matches
+    Match.where('team_home_id = ? OR team_away_id = ?', id, id)
+  end
 end
