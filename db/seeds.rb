25cb8c11f170a6a317a3881bda01bcdb7aa34777
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-07 17:39:01 +0900

    added hash of groups and teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# url = "https://www.uefa.com/imgml/flags/70x70/CZE.png?imwidth=2048%202048w"
+groups = {
+  'Group A' => [
+    { name: 'Italy', abbrev: 'ITA' },
+    { name: 'Switzerland  ', abbrev: 'SUI' },
+    { name: 'Turkey', abbrev: 'TUR' },
+    { name: 'Wales', abbrev: 'WAL' }
+  ],
+  'Group B' => [
+    { name: 'Belgium', abbrev: 'BEL' },
+    { name: 'Denmark', abbrev: 'DEN' },
+    { name: 'Finland', abbrev: 'FIN' },
+    { name: 'Russia', abbrev: 'RUS' }
+  ],
+  'Group C' => [
+    { name: 'Austria', abbrev: 'AUT' },
+    { name: 'Netherlands', abbrev: 'NED' },
+    { name: 'North Macedonia', abbrev: 'MKD' },
+    { name: 'Ukraine', abbrev: 'UKR' }
+  ],
+  'Group D' => [
+    { name: 'Croatia', abbrev: 'CRO' },
+    { name: 'Czech Republic', abbrev: 'CZE' },
+    { name: 'England', abbrev: 'ENG' },
+    { name: 'Scotland', abbrev: 'SCO' }
+  ],
+  'Group E' => [
+    { name: 'Poland', abbrev: 'POL' },
+    { name: 'Slovakia   ', abbrev: 'SVK' },
+    { name: 'Spain', abbrev: 'TUR' },
+    { name: 'Sweden', abbrev: 'WAL' }
+  ],
+  'Group F' => [
+    { name: 'France', abbrev: 'FRA' },
+    { name: 'Germany  ', abbrev: 'GER' },
+    { name: 'Hungary', abbrev: 'HUN' },
+    { name: 'Portugal', abbrev: 'POR' }
+  ]
+}
+puts 'Creating or finding first round...'
+first_round = Round.find_or_create_by(name: 'Group Stage', round: 1)
+puts "...#{Round.count} Total Rounds"
+
+puts 'Creating or finding groups...'
+Group.find_or_create_by(name: groups.keys, round: first_round)
+puts "...#{Group.count} Total Groups"
