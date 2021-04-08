7a9bed92c4415928c73c355eab2959d2dce1a74f
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-08 20:18:09 +0900

    added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :v1 do
+    resources :competitions, only: [] do
+      resources :matches, only: [:index]
+      resources :leagues, only: [:index, :create, :destroy]
+      resources :users, only: [:show]
+      resources :memberships, only: [:create, :destroy]
+    end
+    resources :matches, only: [] do
+      resources :predictions, only: [:create, :update]
+    end
+  end
 end
