ba066c1f3c5c1dede0a56b85fa5382c69c8f84c8
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-08 16:06:10 +0900

    added host url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_user!
+  include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
 
   # Pundit: white-list approach.
