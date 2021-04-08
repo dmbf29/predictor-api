01bd86ddb58316c2f4eaebb8858f22b4dfe8896a
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-08 16:14:41 +0900

    removed duplicated from devise gem. but also removed confirmation email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :trackable, :validatable,
-                    :confirmable, :omniauthable
-            include DeviseTokenAuth::Concerns::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules.
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable # :confirmable
+  include DeviseTokenAuth::Concerns::User
   has_many :memberships, dependent: :destroy
   has_many :competitions, through: :leagues
   has_many :predictions, dependent: :destroy
