d6af8b59928061eb255ea78e48cdba0d80f23967
Trouni Tiet <trouni@gmail.com>
2021-04-09 19:48:02 +0900

    Protect all actions behind authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    sessions: 'auth/devise_token_auth/sessions'
+  }
 end
