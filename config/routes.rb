066b172ff47b7ee1172a1e7c7e8f447dbe5611b3
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-10 14:19:13 +0900

    updated to nest matches inside of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :v1 do
-    resources :competitions, only: [] do
+    resources :users, only: [:show] do
       resources :matches, only: [:index]
+    end
+    resources :competitions, only: [:show] do
       resources :leagues, only: [:index, :create]
-      resources :users, only: [:show]
-      resources :memberships, only: [:create]
     end
     resources :matches, only: [] do
       resources :predictions, only: [:create]
     end
+    resources :leagues, only: [:destroy] do
+      resources :memberships, only: [:create]
+    end
     resources :predictions, only: [:update]
-    resources :leagues, only: [:destroy]
     resources :memberships, only: [:destroy]
   end
 end
