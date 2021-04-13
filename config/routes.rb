13ffd820013a1010745b066eebe7f201c72cdfa3
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-13 20:37:00 +0900

    updated routes for matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :v1 do
-    resources :users, only: [:show] do
-      resources :matches, only: [:index]
-    end
     resources :competitions, only: [:show] do
       resources :leagues, only: [:index, :create]
     end
@@ -13,7 +10,9 @@ Rails.application.routes.draw do
     resources :leagues, only: [:destroy] do
       resources :memberships, only: [:create]
     end
+    resources :matches, only: [:index]
     resources :predictions, only: [:update]
     resources :memberships, only: [:destroy]
+    resources :users, only: [:show]
   end
 end
