291d21d6576d7e520b2b78f0647882061f81f17d
Trouni Tiet <trouni@gmail.com>
2021-04-16 18:03:10 +0900

    Add point calculations for affiliations

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -2,4 +2,13 @@ class Affiliation < ApplicationRecord
   belongs_to :team
   belongs_to :group
   validates_uniqueness_of :team, scope: :group
+
+  def points
+    # Could probably be optimized into a single query
+    team.victories.count * 3 + team.draws.count
+  end
+
+  def goal_diff
+    # TODO
+  end
 end
