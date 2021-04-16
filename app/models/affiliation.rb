8c8eebb52dfd5d5d8f4ec0a973630c651879e75e
Trouni Tiet <trouni@gmail.com>
2021-04-16 22:32:31 +0900

    Move points calculation to Group

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -2,26 +2,4 @@ class Affiliation < ApplicationRecord
   belongs_to :team
   belongs_to :group
   validates_uniqueness_of :team, scope: :group
-
-  def points
-    victories.count * 3 + draws.count
-  end
-
-  def goal_diff
-    victories.sum('ABS(team_home_score - team_away_score)') - defeats.sum('ABS(team_home_score - team_away_score)')
-  end
-
-  private
-
-  def victories
-    team.victories.where(group: group)
-  end
-
-  def defeats
-    team.defeats.where(group: group)
-  end
-
-  def draws
-    team.draws.where(group: group)
-  end
 end
