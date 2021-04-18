58591ab7973181fa9aad1d1c61cebbd08abc311a
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-18 20:20:37 +0900

    updated prediction policy

--- app/policies/prediction_policy.rb
+++ app/policies/prediction_policy.rb
@@ -4,4 +4,18 @@ class PredictionPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
+
+  def update?
+    owner_or_admin?
+  end
+
+  private
+
+  def owner_or_admin?
+    record.user == user || user.admin?
+  end
 end
