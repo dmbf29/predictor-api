a4b904642b9fad4486c796a2e3b97af04d0da77d
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-21 20:20:04 +0900

    renamed league to leaderboard

--- db/migrate/20210421110232_change_leagues_to_leaderboards.rb
+++ db/migrate/20210421110232_change_leagues_to_leaderboards.rb
@@ -1,5 +1,7 @@
 class ChangeLeaguesToLeaderboards < ActiveRecord::Migration[6.1]
   def change
+    remove_reference :memberships, :league, index: true, foreign_key: true
     rename_table :leagues, :leaderboards
+    add_reference :memberships, :leaderboard, foreign_key: true
   end
 end
