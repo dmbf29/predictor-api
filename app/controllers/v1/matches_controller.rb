54c383a3013743021d2acbbc89e41f1188f2df65
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-21 22:06:11 +0900

    updated controller so you can scope by competition

--- app/controllers/v1/matches_controller.rb
+++ app/controllers/v1/matches_controller.rb
@@ -3,7 +3,12 @@ class V1::MatchesController < ApplicationController
   # /matches?competition_id=:id&user_id=:id
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
-    # @competition = Competition.find_by(id: params[:competition_id])
-    @matches = policy_scope(Match)
+    @competition = Competition.find_by(id: params[:competition_id])
+    @matches =
+      if @competition
+        policy_scope(Match).where(group: @competition.groups)
+      else
+        policy_scope(Match)
+      end
   end
 end
