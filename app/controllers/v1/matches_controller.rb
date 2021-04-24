03028314c94a405a204d484f2c540309022d181b
Trouni Tiet <trouni@gmail.com>
2021-04-24 20:48:25 +0900

    Add teams and refactor view

--- app/controllers/v1/matches_controller.rb
+++ app/controllers/v1/matches_controller.rb
@@ -1,12 +1,16 @@
 class V1::MatchesController < ApplicationController
-
   # /matches?competition_id=:id&user_id=:id
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
     @competition = Competition.find_by(id: params[:competition_id])
     @matches =
       if @competition
-        policy_scope(Match).where(group: @competition.groups).order(:kickoff_time)
+        policy_scope(Match).joins(:team_away, :team_home)
+                           .left_outer_joins(:predictions)
+                           .includes(:team_away, :team_home, :predictions)
+                           .where(group: @competition.groups)
+                           .where('predictions.user_id = ? OR predictions.user_id IS NULL', @user.id)
+                           .order(:kickoff_time)
       else
         policy_scope([:user, Match]).order(:kickoff_time)
       end
