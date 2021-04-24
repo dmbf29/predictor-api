e2b08a2575b414da198b8efd927039f8ff0867db
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-24 19:12:47 +0900

    added unique constraint on password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_110232) do
+ActiveRecord::Schema.define(version: 2021_04_24_101146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
