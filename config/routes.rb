025dad23f9ea71d3ef850b68fa23df7d320bc07f
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-24 19:41:51 +0900

    added create method

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
       resources :memberships, only: [:create, :destroy]
     end
     resources :users, only: [:show]
+    get 'join/:password', to: 'memberships#create'
   end
 end
