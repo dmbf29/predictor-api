72eeb55d1653df86417b36f9951258bc31fe2c09
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-29 22:01:54 +0900

    rendering the leaderboard when joining one"

--- app/controllers/v1/memberships_controller.rb
+++ app/controllers/v1/memberships_controller.rb
@@ -6,7 +6,7 @@ class V1::MembershipsController < ApplicationController
     @membership.user = current_user
     authorize @membership
     if @membership.save
-      render :show, status: :created
+      render 'leaderboards/show', status: :created
     else
       render_error(@membership)
     end
