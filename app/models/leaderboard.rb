4961d6b98ff8abf8d2cde498980c7f6be6201bfe
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-29 21:35:24 +0900

    added user associations since leaderboard wasnt including owner

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -2,9 +2,11 @@ class Leaderboard < ApplicationRecord
   belongs_to :user
   belongs_to :competition
   has_many :memberships, dependent: :destroy
-  has_many :users, through: :memberships
   validates :name, presence: true
-  # TODO: Think about how users join groups
-  # validates :password, presence: true
   has_secure_token :password
+
+  def users
+    User.includes(:memberships).where(memberships: { leaderboard: self }).or(User.where(id: users))
+  end
 end
+
