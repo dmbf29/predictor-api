2951aa618e9b2f831d040f0a227a46e9e0a3bb06
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-30 12:36:09 +0900

    added user policy

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :leaderboards, only: [:destroy], shallow: true do
       resources :memberships, only: [:create, :destroy]
     end
-    resources :users, only: [:show]
+    resources :users, only: [:show, :update]
     get 'join/:password', to: 'memberships#create'
   end
 end
