b799eee4c48c1642b1ee4e589a5305a988cbe730
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-01 17:49:50 +0900

    updated leaderboard users

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -2,11 +2,11 @@ class Leaderboard < ApplicationRecord
   belongs_to :user
   belongs_to :competition
   has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
   validates :name, presence: true
   has_secure_token :password
 
   def users
-    User.includes(:memberships).where(memberships: { leaderboard: self }).or(User.where(id: user))
+    super().or(User.where(id: user))
   end
 end
-
