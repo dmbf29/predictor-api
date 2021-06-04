51c378f86f68e35040c290e352c46529da10e72b
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-04 22:22:22 +0900

    upload working

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,8 +1,15 @@
+require 'open-uri'
+
 class V1::UsersController < ApplicationController
   def update
     @user = current_user
     authorize @user
-    if @user.update(prediction_params)
+
+    if params[:user][:photo_url]
+      file = URI.open(params[:user][:photo_url])
+      @user.photo.attach(io: file, filename: 'profile.png', content_type: 'image/png')
+      render :show
+    elsif @user.update(prediction_params)
       render :show
     else
       render_error(@user)
