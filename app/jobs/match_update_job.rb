c6fc6fbb0dfda6e4c3dae3b160d36d4e305e49c7
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 19:49:03 +0900

    updating matches in job

--- app/jobs/match_update_job.rb
+++ app/jobs/match_update_job.rb
@@ -2,14 +2,11 @@ class MatchUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(competition_id)
-    counter = 0
     competition = Competition.find(competition_id)
     url_to_update = "https://livescore-api.com/api-client/fixtures/matches.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition.api_id}"
     while url_to_update
       url_to_update = update_matches(url_to_update)
-      counter += 1
     end
-    counter
   end
 
   def get_home_team(match_info)
@@ -28,13 +25,11 @@ class MatchUpdateJob < ApplicationJob
       match = Match.find_by(id: match_info['id']) || Match.find_by(team_home: get_home_team(match_info), team_away: get_away_team(match_info))
       next unless match # knock-out rounds with no teams
 
-      p match['id']
-      # match.id = match['id'])
-      # match.kickoff_time =
-      # team_home_score = nil
-      # team_away_score = nil
-      # status = "upcoming"
+      match.id = match_info['id']
+      match.location = match_info['location']
+      match.kickoff_time = DateTime.parse("#{match_info['date']} #{match_info['time']}")
+      match.save
     end
-    p parsed_response['next_page']
+    return parsed_response['next_page']
   end
 end
