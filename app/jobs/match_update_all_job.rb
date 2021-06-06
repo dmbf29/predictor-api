f698395b5afbc74221fdf917fb48c554630ce43f
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 18:49:48 +0900

    added api_id to competition

--- app/jobs/match_update_all_job.rb
+++ app/jobs/match_update_all_job.rb
@@ -2,8 +2,10 @@ class MatchUpdateAllJob < ApplicationJob
   queue_as :default
 
   def perform
-    competition_id = 387
-    response = HTTParty.get("http://livescore-api.com/api-client/countries/list.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_id}").body
-    countries = JSON.parse(response)['data']['country']
+    competitions = Competition.on_going
+    competitions.each do |competition|
+      response = HTTParty.get("http://livescore-api.com/api-client/scores/history.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition.api_id}").body
+      countries = JSON.parse(response)['data']['country']
+    end
   end
 end
