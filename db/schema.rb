ef1c8c91325125cf768e9ac4a3a976e24fd4d771
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 19:55:49 +0900

    created a rake task to update match by competition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_093702) do
+ActiveRecord::Schema.define(version: 2021_06_06_100056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 2021_06_06_093702) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "next_match_id"
     t.bigint "round_id"
+    t.integer "api_id"
+    t.string "location"
     t.index ["group_id"], name: "index_matches_on_group_id"
     t.index ["next_match_id"], name: "index_matches_on_next_match_id"
     t.index ["round_id"], name: "index_matches_on_round_id"
