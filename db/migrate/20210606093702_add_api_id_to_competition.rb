059265630b2c0c2cc6dbd76fdd0ed2bae512912a
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 18:39:08 +0900

    created migration to add api_id to competition

---
+++ db/migrate/20210606093702_add_api_id_to_competition.rb
@@ -0,0 +1,12 @@
+class AddApiIdToCompetition < ActiveRecord::Migration[6.1]
+  def change
+    add_column :competitions, :api_id, :integer
+    Competition.find_each do |competition|
+      next if api_id
+
+      # Live-Score API ID for the the Euros
+      competition.api_id = 387
+      competition.save
+    end
+  end
+end
