298b964918919b63dab362819f90e18d47c919b7
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 19:00:43 +0900

    iterating over the matches

--- app/jobs/match_update_all_job.rb
+++ app/jobs/match_update_all_job.rb
@@ -5,7 +5,11 @@ class MatchUpdateAllJob < ApplicationJob
     competitions = Competition.on_going
     competitions.each do |competition|
       response = HTTParty.get("http://livescore-api.com/api-client/scores/history.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition.api_id}").body
-      countries = JSON.parse(response)['data']['country']
+      matches = JSON.parse(response)['data']['match']
+      matches.each do |match|
+        p match
+        puts
+      end
     end
   end
 end
