88d21d84346fb7b2e4e1d2ebf9d17518312a453a
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-06 19:39:44 +0900

    iterating over matches from api

--- app/jobs/match_update_job.rb
+++ app/jobs/match_update_job.rb
@@ -12,26 +12,29 @@ class MatchUpdateJob < ApplicationJob
     counter
   end
 
-  def get_home_team(match)
-    Team.find_by(name: match['home_name'])
+  def get_home_team(match_info)
+    Team.find_by(name: match_info['home_name'])
   end
 
-  def get_away_team(match)
-    Team.find_by(name: match['away_name'])
+  def get_away_team(match_info)
+    Team.find_by(name: match_info['away_name'])
   end
 
   def update_matches(url)
     response = HTTParty.get(url).body
-    matches = JSON.parse(response)['data']['fixtures']
-    matches.each do |match|
-      match = Match.find_by(id: match['id']) || Match.find_by(team_home: get_home_team(match), team_away: get_away_team(match))
-      p match
+    parsed_response = JSON.parse(response)['data']
+    matches = parsed_response['fixtures']
+    matches.each do |match_info|
+      match = Match.find_by(id: match_info['id']) || Match.find_by(team_home: get_home_team(match_info), team_away: get_away_team(match_info))
+      next unless match # knock-out rounds with no teams
+
+      p match['id']
       # match.id = match['id'])
       # match.kickoff_time =
       # team_home_score = nil
       # team_away_score = nil
       # status = "upcoming"
     end
-    return response['data']['next_page']
+    p parsed_response['next_page']
   end
 end
