a2cee6fb030b3cb213e1f9b58c76a19c7f49e50c
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-09 14:27:23 +0900

    user can delete an empty leaderboard or remove their membership from a non-empty one

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -9,4 +9,21 @@ class Leaderboard < ApplicationRecord
   def users
     User.includes(:memberships).where(memberships: { leaderboard: self }).or(User.where(id: user))
   end
+
+  def leave(current_user)
+    if user == current_user
+      # remove if empty OR transfer
+      memberships.any? ? transfer_ownership : destroy
+    elsif (membership = memberships.find_by(user: current_user))
+      # remove membership if just a regular member
+      membership.destroy
+    end
+  end
+
+  def transfer_ownership
+    membership = memberships.first
+    membership.destroy
+    self.user = membership.user
+    save
+  end
 end
