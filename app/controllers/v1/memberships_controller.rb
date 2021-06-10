a13e80cce31dcb1fabb574914ea7ab29271661f0
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-10 22:48:07 +0900

    previously, it wasnt returning the correct id if user had already joined

--- app/controllers/v1/memberships_controller.rb
+++ app/controllers/v1/memberships_controller.rb
@@ -1,9 +1,9 @@
 class V1::MembershipsController < ApplicationController
   def create
     @leaderboard = Leaderboard.find_by(password: params[:password])
-    @membership = Membership.find_or_initialize_by(user: current_user, leaderboard: @leaderboard)
-    authorize @membership
-    if @membership.save
+    membership = Membership.new(user: current_user, leaderboard: @leaderboard)
+    authorize membership
+    if (@membership = Membership.where(user: current_user, leaderboard: @leaderboard).first_or_create)
       render :show, status: :created
     else
       render_error(@membership)
