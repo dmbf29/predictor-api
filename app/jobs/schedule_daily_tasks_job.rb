35d9e61da8f07d3dd32db1fd24e8ffb2aa2f6fbe
Douglas Berkley <douglasmberkley@gmail.com>
2021-06-12 21:34:01 +0900

    Added missing 'end'

--- app/jobs/schedule_daily_tasks_job.rb
+++ app/jobs/schedule_daily_tasks_job.rb
@@ -6,10 +6,10 @@ class ScheduleDailyTasksJob < ApplicationJob
     competitions.each do |competition|
       matches = competition.matches.where(kickoff_time: Date.today.all_day)
       matches.pluck(:kickoff_time).uniq.each do |kickoff_time|
-
-      # Update right after kickoff and after the game
-      MatchStartedJob.set(wait_until: kickoff_time).perform_later(kickoff_time)
-      MatchUpdateHistoryJob.set(wait_until: kickoff_time + 100.minutes).perform_later(competition.id)
+        # Update right after kickoff and after the game
+        MatchStartedJob.set(wait_until: kickoff_time).perform_later(kickoff_time)
+        MatchUpdateHistoryJob.set(wait_until: kickoff_time + 100.minutes).perform_later(competition.id)
+      end
     end
   end
 end
