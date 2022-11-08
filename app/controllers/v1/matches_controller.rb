4f772cc78cb92bd8a652f1251eab6b7d1799007f
Douglas Berkley <douglasmberkley@gmail.com>
2022-11-08 22:51:13 +0900

    updated TODO notes

--- app/controllers/v1/matches_controller.rb
+++ app/controllers/v1/matches_controller.rb
@@ -2,8 +2,8 @@ class V1::MatchesController < ApplicationController
   # /matches?competition_id=:id&user_id=:id
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
+    # TODO: Why is the FE sending competition_id: '1'???
     # p @competition = Competition.find_by(id: params[:competition_id])
-    # TODO: Why doesn't this work ☝️. It's only giving me the Euros
     @competition = Competition.order(id: :desc).first
     skip_policy_scope
     @matches = @user.matches(competition: @competition)
