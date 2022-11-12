c2b24b01ea02d394bbc953a9a2d9875c529b80ee
Douglas Berkley <douglasmberkley@gmail.com>
2022-11-12 14:02:43 +0900

    went back to the original version of fetching the competition from the FE

--- app/controllers/v1/matches_controller.rb
+++ app/controllers/v1/matches_controller.rb
@@ -2,9 +2,7 @@ class V1::MatchesController < ApplicationController
   # /matches?competition_id=:id&user_id=:id
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
-    # TODO: Why is the FE sending competition_id: '1'???
-    # p @competition = Competition.find_by(id: params[:competition_id])
-    @competition = Competition.order(id: :desc).first
+    p @competition = Competition.find_by(id: params[:competition_id])
     skip_policy_scope
     @matches = @user.matches(competition: @competition)
   end
