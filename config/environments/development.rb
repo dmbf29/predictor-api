d2d52c630b1dbfde83745e5593cce09f140af5d2
Trouni <34345789+trouni@users.noreply.github.com>
2022-11-20 20:55:50 +0900

    Add sendgrid SMTP (#92)
    * Add sendgrid SMTP
    
    * Add letter opener and update sender email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
