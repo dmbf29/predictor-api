b5addcd795dd15065546a906ba2bf14aafb86418
Trouni <34345789+trouni@users.noreply.github.com>
2022-11-20 20:55:50 +0900

    Add sendgrid SMTP (#92)
    * Add sendgrid SMTP
    
    * Add letter opener and update sender email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 Devise.setup do |config|
   # The e-mail address that mail will appear to be sent from
   # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
-  config.mailer_sender = "support@example.com"
+  config.mailer_sender = "hello@octacle.app"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
