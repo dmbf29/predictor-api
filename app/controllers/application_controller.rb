f70bcff95f45cfab39cccd19c410ba17d3989160
Trouni Tiet <trouni@gmail.com>
2022-11-23 12:47:45 +0900

    Fix deprecation warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include Pundit
+  include Pundit::Authorization
 
   before_action :authenticate_user!, unless: :token_auth_controller?
 
