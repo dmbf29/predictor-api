b3758d8bc2b8cfb356b39b5c2afaf40691849ab3
Trouni Tiet <trouni@gmail.com>
2022-11-23 12:48:05 +0900

    Remove inspect

--- app/controllers/v1/matches_controller.rb
+++ app/controllers/v1/matches_controller.rb
@@ -2,7 +2,7 @@ class V1::MatchesController < ApplicationController
   # /matches?competition_id=:id&user_id=:id
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
-    p @competition = Competition.find_by(id: params[:competition_id])
+    @competition = Competition.find_by(id: params[:competition_id])
     skip_policy_scope
     @matches = @user.matches(competition: @competition)
   end
