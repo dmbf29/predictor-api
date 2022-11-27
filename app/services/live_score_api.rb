d17af5cfbb623fd4ae2631d900c1b1f927b627f5
Douglas Berkley <douglasmberkley@gmail.com>
2022-11-27 14:39:53 +0900

    added api url

--- app/services/live_score_api.rb
+++ app/services/live_score_api.rb
@@ -1,9 +1,14 @@
 class LiveScoreApi
-  def self.matches_future_url(competition_id)
-    "https://livescore-api.com/api-client/fixtures/matches.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_id}"
+  def self.matches_future_url(competition_api_id)
+    "https://livescore-api.com/api-client/fixtures/matches.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_api_id}"
   end
 
-  def self.matches_history_url(competition_id)
-    "http://livescore-api.com/api-client/scores/history.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_id}"
+  def self.matches_history_url(competition_api_id)
+    "http://livescore-api.com/api-client/scores/history.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_api_id}"
+  end
+
+  def self.matches_live_url(competition_api_id)
+    "http://livescore-api.com/api-client/scores/history.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_api_id}"
+    "https://livescore-api.com/api-client/scores/live.json?key=#{ENV['LIVE_SCORE_KEY']}&secret=#{ENV['LIVE_SCORE_SECRET']}&competition_id=#{competition_api_id}"
   end
 end
