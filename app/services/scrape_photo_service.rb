c1561f72791e30615792529ba0e062bb5454b908
Douglas Berkley <douglasmberkley@gmail.com>
2022-11-30 19:14:05 +0900

    added options to the browser. upgraded webdrivers. changed the url

--- app/services/scrape_photo_service.rb
+++ app/services/scrape_photo_service.rb
@@ -9,10 +9,10 @@ class ScrapePhotoService
   end
 
   def call
-    url = "https://www.fifa.com/fifaplus/en/tournaments/mgstens/worldcup/qatar2022/teams/#{competition.teams.sample.name.split.join('-').downcase}/squad"
-    # browser = Watir::Browser.new :chrome, args: %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222]
+    url = "https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022/teams/#{competition.teams.sample.name.split.join('-').downcase}/squad"
     browser = Watir::Browser.new :chrome, options: { args: %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222] }
     browser.goto url
+    puts "Going to: #{url}"
     sleep(15)
     html_doc = Nokogiri::HTML.parse(browser.html)
     main_div = html_doc.search('main section')[2]
