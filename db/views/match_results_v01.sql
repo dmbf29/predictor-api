47ad739f840302244c9a0b7db0042f6a2f3cf919
Trouni Tiet <trouni@gmail.com>
2022-12-05 19:25:37 +0900

    Fix queries in views

--- db/views/match_results_v01.sql
+++ db/views/match_results_v01.sql
@@ -1,91 +1,59 @@
 WITH match_rounds AS (
-	SELECT
+	SELECT 
 		m.id AS match_id,
 		(
 			CASE
-				WHEN m.round_id IS NOT NULL THEN m.round_id
-				ELSE g.round_id
+			WHEN m.round_id IS NOT NULL THEN m.round_id 
+			ELSE g.round_id
 			END
-		) AS round_id
-	FROM
-		matches m
-		LEFT JOIN groups g ON m.group_id = g.id
-		LEFT JOIN rounds r ON m.round_id = r.id
+		) AS round_id,
+		(
+			CASE
+			WHEN m.round_id IS NOT NULL THEN mr.competition_id  
+			ELSE gr.competition_id 
+			END
+		) AS competition_id
+	FROM matches m
+	LEFT JOIN groups g ON m.group_id = g.id
+	LEFT JOIN rounds mr ON m.round_id = mr.id
+	LEFT JOIN rounds gr ON g.round_id = gr.id
 )
 SELECT
-	m.id AS match_id,
-	m.group_id,
-	r.id AS round_id,
+	matches.id AS match_id,
+	mr.round_id,
 	r.competition_id,
-	m.kickoff_time,
-	m.team_home_score,
-	m.team_away_score,
-	m.status,
-	m.team_away_id,
-	m.team_home_id,
-	m.created_at,
-	m.updated_at,
-	m.next_match_id,
-	m.api_id,
-	m.location,
-	m.team_home_et_score,
-	m.team_away_et_score,
-	m.team_home_ps_score,
-	m.team_away_ps_score,
+	l.id AS leaderboard_id,
+	matches.status,
+	matches.group_id,
+	matches.team_away_id,
+	matches.team_home_id,
+	matches.next_match_id,
 	(
 		CASE
-			WHEN (
-				status = 'upcoming'
-				OR status = 'started'
-			) THEN NULL
-			WHEN (
-				team_home_score = team_away_score
-				AND team_home_et_score = team_away_et_score
-				AND team_home_ps_score = team_away_ps_score
-			) THEN 'draw'
-			WHEN (
-				team_home_score > team_away_score
-				OR (
-					team_home_et_score IS NOT NULL
-					AND team_home_et_score > team_away_et_score
-				)
-				OR (
-					team_home_ps_score IS NOT NULL
-					AND team_home_ps_score > team_away_ps_score
-				)
-			) THEN 'home'
-			ELSE 'away'
+	    WHEN (status = 'upcoming' OR status = 'started') THEN NULL
+	    WHEN (
+        team_home_et_score IS NULL AND
+        team_away_et_score IS NULL AND
+        team_home_ps_score IS NULL AND
+        team_away_ps_score IS NULL AND
+		    team_home_score = team_away_score
+	  	) THEN 'draw'
+	    WHEN (
+		    team_home_score > team_away_score OR
+		    (team_home_et_score IS NOT NULL AND team_home_et_score > team_away_et_score) OR
+		    (team_home_ps_score IS NOT NULL AND team_home_ps_score > team_away_ps_score)
+		) THEN 'home'
+		ELSE 'away'
 		END
 	) AS winning_side,
-	(
-		CASE
-			WHEN (
-				status = 'upcoming'
-				OR status = 'started'
-			) THEN NULL
-			WHEN (
-				team_home_score = team_away_score
-				AND team_home_et_score = team_away_et_score
-				AND team_home_ps_score = team_away_ps_score
-			) THEN NULL
-			WHEN (
-				team_home_score > team_away_score
-				OR (
-					team_home_et_score IS NOT NULL
-					AND team_home_et_score > team_away_et_score
-				)
-				OR (
-					team_home_ps_score IS NOT NULL
-					AND team_home_ps_score > team_away_ps_score
-				)
-			) THEN team_home_id
-			ELSE team_away_id
-		END
-	) AS winner_id,
 	r.number AS round_number,
 	r.points AS points,
-	r.name AS round_name
-FROM
-	matches m
-	JOIN match_rounds mr ON mr.match_id = m.id
-	JOIN rounds r ON mr.round_id = r.id
+	r.name AS round_name,
+	ARRAY(SELECT user_id FROM predictions p JOIN matches m ON m.id = p.match_id WHERE m.id = matches.id AND p.choice = 'home') as predicted_home,
+	ARRAY(SELECT user_id FROM predictions p JOIN matches m ON m.id = p.match_id WHERE m.id = matches.id AND p.choice = 'draw') as predicted_draw,
+	ARRAY(SELECT user_id FROM predictions p JOIN matches m ON m.id = p.match_id WHERE m.id = matches.id AND p.choice = 'away') as predicted_away
+FROM matches
+JOIN match_rounds mr ON mr.match_id = matches.id
+JOIN rounds r ON mr.round_id = r.id
+JOIN leaderboards l ON l.competition_id = mr.competition_id
+ORDER BY matches.id
--- db/views/user_scores_v01.sql
+++ db/views/user_scores_v01.sql
@@ -1,6 +1,9 @@
 WITH prediction_scores AS (
 	SELECT
-		p.id AS prediction_id,
+		DISTINCT p.id AS prediction_id,
+		p.user_id,
+		p.match_id,
+		mr.competition_id,
 		(
 			CASE
 			WHEN mr.status = 'finished' AND p.choice IS NOT NULL THEN TRUE
@@ -21,23 +24,38 @@ WITH prediction_scores AS (
 		) AS prediction_score
 	FROM predictions p
 	LEFT JOIN match_results mr ON mr.match_id = p.match_id
+), prediction_numbers AS (
+	SELECT
+		u.id AS user_id,
+		ps.competition_id,
+		SUM(ps.prediction_score) AS score,
+		COUNT(DISTINCT ps.prediction_id) AS total_predictions,
+		(
+			SELECT COUNT(DISTINCT ps2.prediction_id)
+			FROM prediction_scores ps2
+			WHERE ps2.completed IS TRUE
+			GROUP BY ps2.user_id, ps2.competition_id
+			HAVING ps2.user_id = u.id AND ps2.competition_id = ps.competition_id
+		) AS completed_predictions,
+		(
+			SELECT COUNT(DISTINCT ps2.prediction_id)
+			FROM prediction_scores ps2
+			WHERE ps2.correct IS TRUE
+			GROUP BY ps2.user_id, ps2.competition_id
+			HAVING ps2.user_id = u.id AND ps2.competition_id = ps.competition_id
+		) AS correct_predictions
+	FROM users u
+	LEFT JOIN prediction_scores ps ON ps.user_id = u.id
+	GROUP BY u.id, ps.competition_id
 )
-SELECT
-	u.id AS user_id,
-	mr.competition_id,
-	SUM(ps.prediction_score) AS score,
-	COUNT(ps.prediction_id) AS total_predictions,
-	COUNT(CASE WHEN ps.completed THEN 1 END) AS completed_predictions,
-	COUNT(CASE WHEN ps.correct THEN 1 END) AS correct_predictions,
-	(
-		CASE
-		WHEN COUNT(CASE WHEN ps.completed THEN 1 END) > 0 THEN COUNT(CASE WHEN ps.correct THEN 1 END) * 1.0 / COUNT(CASE WHEN ps.completed THEN 1 END)
-		ELSE NULL 
-		END
-	) AS accuracy
-FROM users u 
-LEFT JOIN predictions p ON p.user_id = u.id 
-LEFT JOIN prediction_scores ps ON ps.prediction_id = p.id
-LEFT JOIN match_results mr ON mr.match_id  = p.match_id
-GROUP BY u.id, mr.competition_id
-ORDER BY u.id
+SELECT DISTINCT ON (ps.user_id, ps.competition_id)
+	ps.user_id,
+	ps.competition_id,
+	pn.score,
+	pn.total_predictions,
+	pn.completed_predictions,
+	pn.correct_predictions,
+	ROUND(pn.correct_predictions * 1.0 / NULLIF(pn.total_predictions, 0), 3) AS accuracy
+FROM prediction_scores ps
+JOIN prediction_numbers pn ON pn.user_id = ps.user_id AND pn.competition_id = ps.competition_id
+ORDER BY ps.user_id
