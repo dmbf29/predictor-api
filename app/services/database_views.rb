59f8c9c89affe7c33dcee8c08d9da7bb4cb5e426
Douglas Berkley <douglasmberkley@gmail.com>
2026-04-27 14:41:43 +0900

    dont need anymore

--- app/services/database_views.rb
+++ app/services/database_views.rb
@@ -7,7 +7,7 @@ class DatabaseViews
 
   def self.deactivate_callback
     MODELS.each do |model|
-      model.skip_callback(:commit, :after, :refresh_materialized_views, raise: false)
+      model.skip_callback(:commit, :after, :refresh_materialized_views)
     end
   end
 
