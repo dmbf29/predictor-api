e7eec08463214259c288ea4aa46d5d3d8412435e
Douglas Berkley <douglasmberkley@gmail.com>
2026-06-11 19:49:02 +0900

    this will auto check if in global when joining a new group

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,10 +3,21 @@ class Membership < ApplicationRecord
   belongs_to :user
   has_one :competition, through: :leaderboard
   validates_uniqueness_of :user, scope: :leaderboard
+  after_create :auto_join_competition_leaderboards
   after_destroy :update_leaderboard_ownership
   after_commit :refresh_materialized_views
 
   private
+
+  def auto_join_competition_leaderboards
+    return if leaderboard.auto_join?
+
+    DatabaseViews.run_without_callback(then_refresh: true) do
+      leaderboard.competition.leaderboards.auto_join.each do |auto_lb|
+        auto_lb.memberships.find_or_create_by(user: user)
+      end
+    end
+  end
 
   def update_leaderboard_ownership
     return unless user == leaderboard.user
